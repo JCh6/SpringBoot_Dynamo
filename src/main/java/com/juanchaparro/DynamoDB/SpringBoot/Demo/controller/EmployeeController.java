4c36f008feb1a1923f016a685479feda53fdfa39
JCh6 <juanch@outlook.com>
2021-10-06 09:42:22 -0500

    fix pathvariable on EmployeeController

--- src/main/java/com/juanchaparro/DynamoDB/SpringBoot/Demo/controller/EmployeeController.java
+++ src/main/java/com/juanchaparro/DynamoDB/SpringBoot/Demo/controller/EmployeeController.java
@@ -2,11 +2,13 @@ package com.juanchaparro.DynamoDB.SpringBoot.Demo.controller;
 
 import com.juanchaparro.DynamoDB.SpringBoot.Demo.entity.Employee;
 import com.juanchaparro.DynamoDB.SpringBoot.Demo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EmployeeController {
 
+    @Autowired
     private EmployeeRepository employeeRepository;
 
     @PostMapping("/employee")
@@ -15,18 +17,18 @@ public class EmployeeController {
     }
 
     @GetMapping("/employee/{id}")
-    public Employee getEmployee(@PathVariable String employeeId) {
-        return employeeRepository.getEmployeeById(employeeId);
+    public Employee getEmployee(@PathVariable String id) {
+        return employeeRepository.getEmployeeById(id);
     }
 
     @DeleteMapping("/employee/{id}")
-    public String deleteEmployee(@PathVariable String employeeId) {
-        return employeeRepository.delete(employeeId);
+    public String deleteEmployee(@PathVariable String id) {
+        return employeeRepository.delete(id);
     }
 
     @PutMapping("/employee/{id}")
-    public String updateEmployee(@PathVariable String employeeId, @RequestBody Employee employee) {
-        return employeeRepository.update(employeeId, employee);
+    public String updateEmployee(@PathVariable String id, @RequestBody Employee employee) {
+        return employeeRepository.update(id, employee);
     }
 
 }
