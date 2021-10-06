14b9a194d96fbf645a131a228b1214adbad1e5e2
JCh6 <juanch@outlook.com>
2021-10-06 09:05:37 -0500

    add dynamodb configuration

---
+++ src/main/java/com/juanchaparro/DynamoDB/SpringBoot/Demo/config/DynamoDBConfiguration.java
@@ -0,0 +1,52 @@
+package com.juanchaparro.DynamoDB.SpringBoot.Demo.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DynamoDBConfiguration {
+
+    @Value("${aws.dynamodb.region}")
+    private String signingRegion;
+
+    @Value("${aws.dynamodb.endpoint}")
+    private String serviceEndpoint;
+
+    @Value("${aws.dynamodb.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.dynamodb.secretKey}")
+    private String secretKey;
+
+    @Bean
+    public DynamoDBMapper dynamoDBMapper() {
+        return new DynamoDBMapper(buildAmazonDynamoDB());
+    }
+
+    private AmazonDynamoDB buildAmazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder
+                .standard()
+                .withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration(
+                                serviceEndpoint,
+                                signingRegion
+                        )
+                )
+                .withCredentials(
+                        new AWSStaticCredentialsProvider(
+                                new BasicAWSCredentials(
+                                        accessKey,
+                                        secretKey
+                                )
+                        )
+                ).build();
+    }
+
+}
