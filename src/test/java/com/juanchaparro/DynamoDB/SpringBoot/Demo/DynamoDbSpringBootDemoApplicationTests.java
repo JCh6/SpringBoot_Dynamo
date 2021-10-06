81480c1361f4fd157d7f50b0d81779861809c130
JCh6 <juanch@outlook.com>
2021-10-06 08:44:40 -0500

    first commit

---
+++ src/test/java/com/juanchaparro/DynamoDB/SpringBoot/Demo/DynamoDbSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanchaparro.DynamoDB.SpringBoot.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamoDbSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
